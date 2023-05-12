742f489ef3356c99ecfea02cb67e5d8b13e47b74
Dave-Kent <dk.davidkent@gmail.com>
2023-05-12 15:22:52 +0100

    first try with external js file

---
+++ familyStory.js
@@ -0,0 +1,17 @@
+//<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
+
+/*function init()
+{
+    document.getElementById("panel").innerHTML="Hello from external js";
+
+    window.alert("Document Loaded!");
+}
+window.onload = init;
+
+var size = $("#cage").width();
+$("#btn").html(size);
+*/
+$(document).ready(function(){
+    alert("Boo")
+});
