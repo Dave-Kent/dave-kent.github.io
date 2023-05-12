121d92071bb5396f7226a215d0c48821ef359b90
Dave-Kent <dk.davidkent@gmail.com>
2023-05-12 19:12:29 +0100

    link to photos from html

--- familyStory.js
+++ familyStory.js
@@ -1,17 +1,16 @@
-//<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 
 
-/*function init()
+function init()
 {
-    document.getElementById("panel").innerHTML="Hello from external js";
-
-    window.alert("Document Loaded!");
-}
-window.onload = init;
+ //   document.getElementById("panel").innerHTML="Hello from external js";
 
 var size = $("#cage").width();
-$("#btn").html(size);
-*/
-$(document).ready(function(){
-    alert("Boo")
+$("#panel").html(size);
+
+/* $("#btn").cick(function()
+{
+document.getElementById("panel2").innerHTML"CLICK-CLICK";
 });
+*/
+}
+window.onload = init;
