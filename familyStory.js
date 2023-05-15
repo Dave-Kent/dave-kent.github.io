c419820123289a22042f41573f853e00f292680b
Dave-Kent <dk.davidkent@gmail.com>
2023-05-15 19:58:21 +0100

    all contents of 'cage' div loaded from js

--- familyStory.js
+++ familyStory.js
@@ -1,13 +1,24 @@
 
-
+var dadDetail = "Fred Bloggs<br>1952-1999";
 
 $(document).ready(function()
 {
 
+// this bit works, I should be able to construct the contents of the main div and load it this way
+var cageContents = '<button id="dadBtn" style="position:absolute;left:5%;top:25%;">'+dadDetail+'\
+</button><div id="tTop" class="hor"style="width:60%;top:30%;left:10%"></div>\
+      <div id="tDown" class="vert"style="height:50%; top: 30%; left:40%;"></div>\
+      <div class="vert"style="height:10%;top:25%;left:70%"></div>\
+      <div class="vert"style="height:50%;top:50%;left:60%;"></div>'
+
+$("#cage").html(cageContents);
+
+
 var size = $("#cage").width();
 $("#panel").html(size);
 
- $("#btn").click(function(){
+// this bit works, I should be able to set up the click responses like this
+ $("#dadBtn").click(function(){
 $("#panel2").text("CLICK-CLICK");
 
 }); // end of btn click function
