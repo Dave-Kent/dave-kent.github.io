4c7d97da46149f523f7ef6fe7c45ac1b618d5530
Dave-Kent <dk.davidkent@gmail.com>
2023-05-17 18:17:24 +0100

    shortChild function added

--- familyStory.js
+++ familyStory.js
@@ -5,41 +5,45 @@ function shortVertical(x,y)
 {
     return '<div class="vert"style="height:70px;top:'
     + y + 'px;left:'+ x +'px"></div>';
-}
+} // end of shortVertical fn
 function longVertical(x,y)
 {
     return '<div class="vert"style="height:200px;top:'
     + y + 'px;left:'+ x +'px"></div>';
-}
+} // end of longVertical fn
+
 function parents(x,y)
 {
     return '<button id="dadBtn" style="position:absolute;left:'
-    + x +'px;top:'+ y +'px;">'+dadDetail+
+    + (x-215) +'px;top:'+ y +'px;">'+dadDetail+
     '</button><div id="tTop" class="hor"style="width:30px;top:'
-    + (y+30) +'px;left:'+ (x+200) +
+    + (y+30) +'px;left:'+ (x-15) +
     'px"></div><button id="mumBtn" style="position:absolute;left:'
-    + (x+230) +'px;top:'+ y +'px;">'+mumDetail
-    +'</button>'+ shortVertical(x+215,y+30)
+    + (x+15) +'px;top:'+ y +'px;">'+mumDetail
+    +'</button>'+ shortVertical(x,y+30)
+} // end of parents fn
+
+function shortChild(n,x,y,detail)
+{
+    return shortVertical(x,y) + '<button id="kid[n]" style="position:absolute;left:'+(x-100)+
+    'px;top:'+(y+70)+'px;">'+ detail +'</button>'
 }
-// this bit works, I should be able to construct the contents of 
-// the main div and load it this way
-var cageContents = parents(20,50)+ longVertical(300,190)
-
-
 //       <<<<<<<<<<<<<<<<<<<<<<===>>>>>>>>>>>>>>>>>>>>
 $(document).ready(function()
       {
-// !! THE CAGE SIZE NEEDS TO BE FOUND BEFORE THE ITEMS CAN BE 
-// POSITIONED !!!
-var size = $("#cage").width();
+
+var size = $("#mainPanel").width();
+var panelMiddle = size/2
 $("#panel").html(size);
 
-$("#cage").html(cageContents);
+var mainPanelContents = parents(panelMiddle,50) // should put
++ shortChild(0,panelMiddle-50,150,"empty child<br>1980") // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+$("#mainPanel").html(mainPanelContents);
 
 
 // this bit works, I should be able to set up the click 
 // responses like this 
-// MAYBE THE ID '#dadBtn' COULD BE CHANGED TO CLASS!!!??
+
  $("button").click(function(){
 $("#panel2").text("cick once "+this.id);
 
