f4d1ac954fedc256f2d92997d1b96d7819f632c0
Dave-Kent <dk.davidkent@gmail.com>
2023-05-29 12:02:36 +0100

    layout centred

--- familyStory.js
+++ familyStory.js
@@ -28,73 +28,138 @@ const families = [
     { // Family 2
     dad : { name: "Bob", dates: "1900-1988", vektr:0},
     mum : { name: "Sarah", dates: "1910-1999", vektr:1},
-    kids : [{ name: "John", dates: "1930-2000", vektr:1},
-    { name: "Jane", dates: "1935-2010", vektr:0},
-    { name: "Lisa", dates: "1938", vektr:1}//NEXT
+    kids : [{ name: "John", dates: "1930-2000", vektr:3},
+    { name: "Jane", dates: "1935-2010", vektr:3},
+    { name: "Lisa", dates: "1938", vektr:3}//NEXT
     ]
-    }//NEXT
+    },
+    { // Family 3
+    dad : { name: "Six Dad", dates: "1-6", vektr:1},
+    mum : { name: "Six Mum", dates: "2-7", vektr:0},
+    kids : [{ name: "One Child", dates: "1-1", vektr:2},
+    { name: "Two Child", dates: "2-2", vektr:2},
+    { name: "Three Child", dates: "3-3", vektr:0},
+    { name: "Four Child", dates: "4-4", vektr:1},
+    { name: "Five Child", dates: "5-5", vektr:2},
+    { name: "Six Child", dates: "6-6", vektr:0}
+    ]
+    }//NEXT//NEXT
 ]
-
-function shortVertical(x,y) // draws a 70px vertical line down
+// draws a vertical line down from x,y position by h pixels
+function shortVertical(x,y,h) 
 {
-    return '<div class="vert"style="height:70px;top:'
+    return '<div class="vert"style="height:'+h+'px;top:'
     + y + 'px;left:'+ x +'px"></div>';
 } // end of shortVertical fn
 
-function longVertical(x,y) // draws a 200px vertical line down
-{
-    return '<div class="vert"style="height:200px;top:'
-    + y + 'px;left:'+ x +'px"></div>';
-} // end of longVertical fn
-
-// draws a complete parent block 430px wide,100px high. (nFamily is the index of
-// the families array, 'this' refers to the current button object).
-function parents(x,y,nFamily)
+// draws a complete parent block (400+hSpace)px wide,100px high. (nFamily is the index of
+// the families array, 'this' refers to the current button object, 
+// hSpace is the gap between the dad and the mum buttons).
+function parents(x,y,hSpace,nFamily)
 {
     return '<button id="dad" onclick="klik(this,'+nFamily+')" style="position:absolute;left:'
-    + (x-215) +'px;top:'+ y +'px;">'+families[nFamily].dad.name+
+    + (x-200-hSpace/2) +'px;top:'+ y +'px;">'+families[nFamily].dad.name+
     '<br>'+families[nFamily].dad.dates+
     '</button><div id="tTop" class="hor"style="width:30px;top:'
-    + (y+30) +'px;left:'+ (x-15) +
+    + (y+30) +'px;left:'+ (x-hSpace/2) +
     'px"></div><button id="mum" onclick="klik(this,'+nFamily+')"style="position:absolute;left:'
-    + (x+15) +'px;top:'+ y +'px;">'+families[nFamily].mum.name+
+    + (x+hSpace/2) +'px;top:'+ y +'px;">'+families[nFamily].mum.name+
     '<br>'+families[nFamily].mum.dates
-    +'</button>'+ shortVertical(x,y+30)
+    +'</button>'+ shortVertical(x,y+30,70)
 } // end of parents fn
 
-// draws a single child button on a short vertical 130px high, 200px wide
-function shortChild(n,x,y,nFamily)
+// draws a single child button on a vertical lolly stick (ht)px high, 200px wide
+function shortChild(n,x,y,ht,nFamily)
 {
-    return shortVertical(x,y) + '<button id="kids['+n+']" onclick="kliq('
+    return shortVertical(x,y,ht) + '<button id="kids['+n+']" onclick="kliq('
     +n+','+  nFamily+')" style="position:absolute;left:'+(x-100)+
-    'px;top:'+(y+70)+'px;">'+families[nFamily].kids[n].name+'<br>'
+    'px;top:'+(y+ht)+'px;">'+families[nFamily].kids[n].name+'<br>'
     +families[nFamily].kids[n].dates+'</button>';
 } //end of shortChild fn
 
 // sets out family tree
 function tree(familyIndex)
 {
-    var panelHalf = 300//$("#window").height()/2; 
-    var panelMiddle = $("#mainPanel").width()/2;
-    var nkids = families[familyIndex].kids.length;
-
-    $("#panel2").text(nkids); // <<<<<<<<<<<<<<<<<<<< DEBUGGING VARIABLE!!!
+    var panelHalf = window.innerHeight/2-30; // vertical position datum
+    var fullWidth = window.innerWidth;// full width of the visible screen
+    var panelMiddle = fullWidth/2; // centre line
+    
+    var nkids = families[familyIndex].kids.length; // number of children 
+    
+    //$("#panel").text($("#mainPanel").width());
+    //$("#panel2").text(nkids); // <<<<<<<<<<<<<<<<<<<< DEBUGGING VARIABLE!!!
+// console.log("nkids: "+nkids+" panel width: "+$("#mainPanel").width());//<<<
 
 // Loop to set out the children - START
-    var startPoint = panelMiddle-210*(nkids-1)/2;
+
     var allTheKids = '';
-    for(i= 0 ; i < nkids; i++)
+    if (fullWidth > (nkids * 210 + 10)) // wide screen, all children in a row.
     {
-        allTheKids = allTheKids + 
-        shortChild(i,(startPoint+(i*210)),panelHalf,familyIndex);
+        var startPoint = panelMiddle-210*(nkids-1)/2; // horizontal position
+        for(i= 0 ; i < nkids; i++)
+        {
+            allTheKids = allTheKids + 
+            shortChild(i,(startPoint+(i*210)),panelHalf,70,familyIndex);
+        }
+        var crossPiece = '<div id="crossPiece" class="hor"style="width:'
+        +(nkids-1)*210+'px;top:'+ panelHalf +'px;left:'
+        + (panelMiddle-(nkids-1)*105) +'px"></div>';
+        return parents(panelMiddle,(panelHalf-100),30,familyIndex) + allTheKids + crossPiece;
     }
+    else if (fullWidth > (nkids/2*220+10)) // mid-width screen, children in two rows
+    {
+        var startPoint = panelMiddle-220*(nkids-1)/4; // horizontal position
+        for(i=0 ; i < nkids; i++)
+        {
+            if(i&1) // test for odd number
+            {
+                allTheKids = allTheKids +
+                shortChild(i,(startPoint+(i*110)),panelHalf,150,familyIndex);
+            }
+            else
+             {
+                allTheKids = allTheKids +
+                shortChild(i,(startPoint+(i*110)),panelHalf,50,familyIndex);
+            }
+            var crossPiece = '<div id="crossPiece" class="hor"style="width:'
+        +(nkids-1)*110+'px;top:'+ panelHalf +'px;left:'
+        + (panelMiddle-(nkids-1)*55) +'px"></div>' ;          
+        }
+        var space = 30;
+        if (fullWidth<430){space=12}; // move parent buttons closer if space is tight
+        console.log("panelMiddle: "+panelMiddle+" fullWidth: "+fullWidth)
+        return parents(panelMiddle,(panelHalf-100),space,familyIndex) + allTheKids + crossPiece;
+    }
+    else // narrowest screen, children ranked vertically
+    {
+        var space = 30; //standard spacing between parents
+        if (fullWidth<440){space=12}; // move parent buttons closer if space is tight
+        var step = (340+space)/2/(nkids-1); // children appear in a stepped rank
+        // the first child comes below the dad
+        // NB child positioning is from the MIDDLE of the button
+        var kidsStart = panelMiddle-100-space/2;
+        var crossPieceLength = (fullWidth+space)/2; // crosspiece for each child
+        // drawn from the right
+        var crossPieceStart = 25; // a suitable distance from the right side
+        console.log("fullWidth: "+fullWidth+" nkids: "+nkids+" step: "+step); //<<<
+        var topScreen= 30; // topScreen is the gap above the parents buttons
+        for(i=0; i<nkids; i++)
+        {
+            allTheKids = allTheKids + // step down the screen: 65px height of parent block: 100px
+            shortChild(i,kidsStart+step*i,topScreen+70+i*65,0,familyIndex)+ // each child
+            // each crosspiece
+            '<div class="hor"style="width:'+(crossPieceLength-step*i)+'px;top:'
+            +(topScreen+100+i*65)+'px;right:'+crossPieceStart+'px"></div>';
+        }
+        
+        return parents(panelMiddle,topScreen,space,familyIndex)+allTheKids;
+    }
+}
+  
 // Loop to set out the children - END
     
-    var crossPiece = '<div id="crossPiece" class="hor"style="width:'+(nkids-1)*210+'px;top:'
-    + panelHalf +'px;left:'+ (panelMiddle-(nkids-1)*105) +'px"></div>'
-
-    return parents(panelMiddle,(panelHalf-100),familyIndex) + allTheKids + crossPiece;
-}
+    
+    
 
 // Click function for each parent
 // (person object in a family object in the families array)
