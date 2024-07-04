662148718ecd6796786e553653cfa6cc8de0cccf
dave-kent <dk.davidkent@gmail.com>
2024-07-04 21:00:15 +0100

    doodle1 green and blue added

--- dood1.js
+++ dood1.js
@@ -4,10 +4,16 @@ function dood1Init() {
     console.log('dood1Init started');
     canvas = document.getElementById('dood1Canvas');
     ctx = canvas.getContext('2d');
-    drawSquare();
+    ctx.translate(100,100);
+    drawSquare(120,150,'red');
+    ctx.translate(100,100);
+    drawSquare(70,90,'green');
+    ctx.rotate(Math.PI/3);
+    ctx.translate(80,0);
+    drawSquare(140,20,'blue');
 }
-function drawSquare() {
-    ctx.fillStyle='red';
-    ctx.fillRect(10,10,100,100);
+function drawSquare(width,height,colour) {
+    ctx.fillStyle=colour;
+    ctx.fillRect(0,0,width,height);
 
 }
